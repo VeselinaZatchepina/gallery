3b2198fb37c9f956660511b54db54134ad4ba56f
Veselina Zatchepina <techpro20@mail.ru>
2018-04-07 01:48:09 +0300

    feat: resize images

--- MyGallery/app/src/main/java/com/github/veselinazatchepina/mygallery/allphotos/AllPhotosFragment.kt
+++ MyGallery/app/src/main/java/com/github/veselinazatchepina/mygallery/allphotos/AllPhotosFragment.kt
@@ -1,19 +1,29 @@
 package com.github.veselinazatchepina.mygallery.allphotos
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.veselinazatchepina.mygallery.R
+import com.github.veselinazatchepina.mygallery.abstracts.AdapterImpl
+import com.github.veselinazatchepina.mygallery.poko.Photo
+import kotlinx.android.synthetic.main.recycler_view.*
+import kotlinx.android.synthetic.main.recycler_view_image_item.view.*
+import org.jetbrains.anko.support.v4.toast
 
 
 class AllPhotosFragment : Fragment() {
 
+    private var rootView: View? = null
     private val allPhotosViewModel by lazy {
         ViewModelProviders.of(this).get(AllPhotosViewModel::class.java)
     }
+    private lateinit var photosAdapter: AdapterImpl<Photo>
 
     companion object {
         fun createInstance(): AllPhotosFragment {
@@ -26,12 +36,34 @@ class AllPhotosFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        rootView = inflater.inflate(R.layout.recycler_view, container, false)
         allPhotosViewModel.getAllPhotos()
-        return inflater.inflate(R.layout.recycler_view, container, false)
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        createPhotosAdapter()
+        allPhotosViewModel.livePhotos.observe(this, Observer {
+            photosAdapter.update(it?.photosInfo?.photos ?: emptyList())
+            Log.d("PHOTOS", "${it?.photosInfo?.photos?.size}")
+        })
+    }
+
+    private fun createPhotosAdapter() {
+        photosAdapter = AdapterImpl(emptyList<Photo>(),
+                R.layout.recycler_view_image_item,
+                R.layout.recycler_view_error, {
+            if (it.url.isNotEmpty()) {
+                allPhotosViewModel.downloadPhoto(it.url, currentImage)
+            }
+        }, {
+            toast("Click!")
+        }, {
+            toast("LongClick!")
+        })
+        recyclerView.adapter = photosAdapter
+        recyclerView.layoutManager = GridLayoutManager(activity, 2)
     }
 
 
--- MyGallery/app/src/main/java/com/github/veselinazatchepina/mygallery/allphotos/AllPhotosViewModel.kt
+++ MyGallery/app/src/main/java/com/github/veselinazatchepina/mygallery/allphotos/AllPhotosViewModel.kt
@@ -1,9 +1,14 @@
 package com.github.veselinazatchepina.mygallery.allphotos
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.util.Log
+import android.widget.ImageView
+import com.github.veselinazatchepina.mygallery.R
 import com.github.veselinazatchepina.mygallery.data.GalleryRepository
 import com.github.veselinazatchepina.mygallery.data.remote.GalleryRemoteDataSource
+import com.github.veselinazatchepina.mygallery.poko.RecentPhotos
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -17,15 +22,26 @@ class AllPhotosViewModel : ViewModel() {
         GalleryRepository.getInstance(GalleryRemoteDataSource.getInstance())
     }
 
+    var livePhotos = MutableLiveData<RecentPhotos>()
+
     fun getAllPhotos() {
         compositeDisposable.add(galleryDataSource.getAllPhotos()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ photos ->
-                    Log.d("PHOTOS", "${photos.photosInfo.photos.size}")
+                .subscribe({ recentPhotos ->
+                    Log.d("PHOTOS", "OK")
+                    livePhotos.value = recentPhotos
                 }, { error ->
                     Log.d("PHOTOS_ERROR", "${error.printStackTrace()}")
                 }))
+    }
+
+    fun downloadPhoto(url: String, imageView: ImageView) {
+        Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.empty_image)
+                .error(R.drawable.empty_image)
+                .into(imageView)
     }
 
     override fun onCleared() {
